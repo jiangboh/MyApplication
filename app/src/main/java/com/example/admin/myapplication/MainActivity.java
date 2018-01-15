88e3d5d23fb6769f98848cad280c4323f0b35254
jb h <h34154@163.com>
2018-01-15 17:50:28 +0800

    加注释123

--- app/src/main/java/com/example/admin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/admin/myapplication/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //加注释
+        //加注释123
         setContentView(R.layout.activity_main);
     }
 }
